74ec74c9540ad5f692ba325428b34fa49a56ef1b
Alexandru Bereghici <abereghici@tripadvisor.com>
2021-11-29 17:47:15 +0200

    removed unused code

--- app/utils/mdx.tsx
+++ app/utils/mdx.tsx
@@ -1,32 +1,6 @@
 import React from 'react'
 import * as mdxBundler from 'mdx-bundler/client'
-import {buildImageUrl} from 'cloudinary-build-url'
 import {getImageBuilder, getImgProps} from './images'
-
-async function getBlurDataUrl(cloudinaryId: string) {
-  const imageURL = buildImageUrl(cloudinaryId, {
-    transformations: {
-      resize: {width: 100},
-      quality: 'auto',
-      format: 'webp',
-      effect: {
-        name: 'blur',
-        value: '1000',
-      },
-    },
-  })
-  const dataUrl = await getDataUrlForImage(imageURL)
-  return dataUrl
-}
-
-async function getDataUrlForImage(imageUrl: string) {
-  const res = await fetch(imageUrl)
-  const arrayBuffer = await res.arrayBuffer()
-  const base64 = Buffer.from(arrayBuffer).toString('base64')
-  const mime = res.headers.get('Content-Type') ?? 'image/webp'
-  const dataUrl = `data:${mime};base64,${base64}`
-  return dataUrl
-}
 
 function Image({
   cloudinaryId,
