0bebdb541f2ddf6554aa5fbd3ebbccfa2969a39d
Alexandru Bereghici <abereghici@tripadvisor.com>
2021-12-21 17:55:59 +0200

    removed unused lint-staged config

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
-    `npm run format --`,
-    `npm run typecheck --silent --`,
-    `npm run lint --silent --`,
-    `npm run test --silent --watch=false --findRelatedTests --`,
-  ],
-}
