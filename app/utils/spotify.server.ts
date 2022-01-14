5f76b699fb7826fea217390b6dfa0c4a99d95911
= <alexandru.brg@gmail.com>
2022-01-14 14:03:52 +0200

    Updated endpoint for spotify top tracks

--- app/utils/spotify.server.ts
+++ app/utils/spotify.server.ts
@@ -8,7 +8,7 @@ const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN ?? ''
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`
-const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`
+const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=short_term`
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
 
 async function getAccessToken() {
