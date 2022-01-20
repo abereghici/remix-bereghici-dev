24a9c59e992de96aaebd4cf4291dee8bd677bdd7
= <alexandru.brg@gmail.com>
2022-01-20 10:07:00 +0200

    disable cors for sitemap and rss

--- app/other-routes.server.ts
+++ app/other-routes.server.ts
@@ -16,6 +16,7 @@ const pathedRoutes: Record<string, Handler> = {
       headers: {
         'Content-Type': 'application/xml',
         'Content-Length': String(Buffer.byteLength(rss)),
+        'Access-Control-Allow-Origin': '*',
       },
     })
   },
@@ -25,6 +26,7 @@ const pathedRoutes: Record<string, Handler> = {
       headers: {
         'Content-Type': 'application/xml',
         'Content-Length': String(Buffer.byteLength(sitemap)),
+        'Access-Control-Allow-Origin': '*',
       },
     })
   },
