c135194f81d8fab2c3644989c9737feed824c7bd
= <alexandru.brg@gmail.com>
2022-02-04 10:36:39 +0200

    chore: fix deps. added preload to css

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,6 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : []),
   ],
 }
