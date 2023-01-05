57b0f3d7378920a131ea7f1ec223f6a7cf834fb8
Alexandru Bereghici <alexandru.brg@gmail.com>
2023-01-05 11:35:25 +0200

    Remove redis replica client

--- app/utils/redis.server.ts
+++ app/utils/redis.server.ts
@@ -5,47 +5,29 @@ declare global {
   // This prevents us from making multiple connections to the db when the
   // require cache is cleared.
   // eslint-disable-next-line
-  var replicaClient: redis.RedisClient | undefined,
-    primaryClient: redis.RedisClient | undefined
+  var primaryClient: redis.RedisClient | undefined
 }
 
 const REDIS_URL = getRequiredServerEnvVar('REDIS_URL')
-const replica = new URL(REDIS_URL)
-const isLocalHost = replica.hostname === 'localhost'
-const isInternal = replica.hostname.includes('.internal')
+const primary = new URL(REDIS_URL)
+const isLocalHost = primary.hostname === 'localhost'
+const isInternal = primary.hostname.includes('.internal')
 
-const isMultiRegion = !isLocalHost && isInternal
+const PRIMARY_REGION = getRequiredServerEnvVar('PRIMARY_REGION')
 
-const PRIMARY_REGION = isMultiRegion
-  ? getRequiredServerEnvVar('PRIMARY_REGION')
-  : null
-const FLY_REGION = isMultiRegion ? getRequiredServerEnvVar('FLY_REGION') : null
+let primaryClient: redis.RedisClient | null = null
 
-if (FLY_REGION) {
-  replica.host = `${FLY_REGION}.${replica.host}`
+if (!isLocalHost) {
+  primary.host = `${PRIMARY_REGION}.${primary.host}`
 }
 
-const replicaClient = createClient('replicaClient', {
-  url: replica.toString(),
+primaryClient = createClient({
+  url: primary.toString(),
   family: isInternal ? 'IPv6' : 'IPv4',
 })
 
-let primaryClient: redis.RedisClient | null = null
-if (FLY_REGION !== PRIMARY_REGION) {
-  const primary = new URL(REDIS_URL)
-  if (!isLocalHost) {
-    primary.host = `${PRIMARY_REGION}.${primary.host}`
-  }
-  primaryClient = createClient('primaryClient', {
-    url: primary.toString(),
-    family: isInternal ? 'IPv6' : 'IPv4',
-  })
-}
-
-function createClient(
-  name: 'replicaClient' | 'primaryClient',
-  options: redis.ClientOpts,
-): redis.RedisClient {
+function createClient(options: redis.ClientOpts): redis.RedisClient {
+  const name = 'primaryClient'
   let client = global[name]
   if (!client) {
     const url = new URL(options.url ?? 'http://no-redis-url.example.com?weird')
@@ -64,12 +46,9 @@ function createClient(
 
 function get<Value = unknown>(key: string): Promise<Value | null> {
   return new Promise(resolve => {
-    replicaClient.get(key, (err: Error | null, result: string | null) => {
+    primaryClient?.get(key, (err: Error | null, result: string | null) => {
       if (err) {
-        console.error(
-          `REDIS replicaClient (${FLY_REGION}) ERROR with .get:`,
-          err,
-        )
+        console.error(`REDIS ERROR with .get:`, err)
       }
       resolve(result ? (JSON.parse(result) as Value) : null)
     })
@@ -78,15 +57,11 @@ function get<Value = unknown>(key: string): Promise<Value | null> {
 
 function set<Value>(key: string, value: Value): Promise<'OK'> {
   return new Promise(resolve => {
-    replicaClient.set(
+    primaryClient?.set(
       key,
       JSON.stringify(value),
       (err: Error | null, reply: 'OK') => {
-        if (err)
-          console.error(
-            `REDIS replicaClient (${FLY_REGION}) ERROR with .set:`,
-            err,
-          )
+        if (err) console.error(`REDIS ERROR with .set:`, err)
         resolve(reply)
       },
     )
@@ -95,18 +70,9 @@ function set<Value>(key: string, value: Value): Promise<'OK'> {
 
 function del(key: string): Promise<string> {
   return new Promise(resolve => {
-    // fire and forget on primary, we only care about replica
-    primaryClient?.del(key, (err: Error | null) => {
+    primaryClient?.del(key, (err: Error | null, result: number | null) => {
       if (err) {
         console.error('Primary delete error', err)
-      }
-    })
-    replicaClient.del(key, (err: Error | null, result: number | null) => {
-      if (err) {
-        console.error(
-          `REDIS replicaClient (${FLY_REGION}) ERROR with .del:`,
-          err,
-        )
         resolve('error')
       } else {
         resolve(`${key} deleted: ${result}`)
